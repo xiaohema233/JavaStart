1153e23fbd5f6c467281f57a40999ac9e5b5cc22
xiaohema233 <33052287+xiaohema233@users.noreply.github.com>
2020-03-02 08:10:05 +0800

    Day4 switch for while dow...while 循环嵌套 跳转语句

---
+++ 毕向东基础/Day4/02_循环结构(for语句)/Demo.java
@@ -0,0 +1,17 @@
+/*
+	需求：请在控制台输出10次"HelloWorld"
+*/
+class Demo {
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("HelloWorld");
+	}
+}
