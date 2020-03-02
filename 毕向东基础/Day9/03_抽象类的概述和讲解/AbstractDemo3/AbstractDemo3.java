46d761a8f9eff3b42ef015f0a9922c265cfadc3e
xiaohema233 <33052287+xiaohema233@users.noreply.github.com>
2020-03-02 16:53:59 +0800

    Day9 final关键字 多态 抽象类 接口

---
+++ 毕向东基础/Day9/03_抽象类的概述和讲解/AbstractDemo3/AbstractDemo3.java
@@ -0,0 +1,35 @@
+/*
+一个类如果没有抽象方法，可不可以定义为抽象类?如果可以，有什么意义?
+	A:可以。
+	B:不让创建对象。
+
+abstract不能和哪些关键字共存?
+	private	冲突
+	final	冲突	
+	static	无意义
+*/
+abstract class Fu {
+	//public abstract void show();
+	//非法的修饰符组合: abstract和private
+	//private abstract void show();
+	
+	//非法的修饰符组合
+	//final abstract void show();	
+	
+	//非法的修饰符组合
+	static abstract void show();
+	
+	public static void method() {
+		System.out.println("method");
+	}
+}
+
+class Zi extends Fu {
+	public void show() {}
+}
+
+class AbstractDemo3 {
+	public static void main(String[] args) {
+		Fu.method();
+	}
+}
